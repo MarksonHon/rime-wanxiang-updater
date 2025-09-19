45331a27f72d2be716eb21d4a6e6134c06d3e9b1
Markson Hon <markson@fedora.lan>
2025-09-20 00:18:48 +0800

    remove en readme

--- README.md
+++ README.md
@@ -1,34 +1,33 @@
-# Wanxiang Input Scheme Updater
+# 万象输入方案更新器
 
-## Usage
+## 使用方式
 
-Open a Terminal and input:
+打开一个终端然后运行：
 
 ```sh
 sh -c "$(curl -Ls https://github.com/MarksonHon/rime-wanxiang-updater/raw/refs/heads/main/updater.sh)"
 ```
 
-## System Requirements
+## 运行环境要求
 
-Requires `curl`, `unzip`, and a POSIX-compliant shell interpreter (e.g., `bash`) installed on your computer. These tools are freely available and functional on most operating systems.
+要求计算机上安装了 `curl`、`unzip` 与一个符合 POSIX Shell 语言标准的解析器，例如 `bash`。这些工具在大多数操作系统上都能免费获取且正常运行。
 
 ### Linux
 
-Most Linux systems can execute this script correctly. If encountering issues, verify the installation of the three aforementioned packages.
+大多数 Linux 系统可以正确执行此脚本，如果运行异常，请注意以上三个软件包是否安装。
 
 ### macOS
 
-macOS's built-in `zsh` should theoretically run this script directly. If it fails, install `bash` via `Homebrew`.
+macOS 自带的 `zsh`，理论上可以直接运行此脚本。如果运行失败，建议从 `Homebrew` 安装 `bash`。
 
 ### Windows
 
-Windows requires the [Git for Windows client][def0]. Run this script within `git-bash`.
-
-Alternatively, you can install `busybox` via `scoop` and run the script within `ash`. However, this method has not been tested and its functionality is not guaranteed.
+Windows 需要安装 [Windows 版本的 Git][def0]，并在 `git-bash` 内运行此脚本；或者，使用[具有 Unicode 支持的 busybox][def2] 的 `ash` 来运行此脚本。
 
 ### Android
 
-Requires [Termux][def1]. After installing `curl`, run this script. Grant Termux full file management permissions.
+需要使用 [Termux][def1]，并在安装 `curl` 之后运行本脚本，需要授予 Termux 完全的文件管理权限。
 
 [def0]: https://git-scm.com/downloads/win
 [def1]: https://termux.dev/cn
+[def2]: https://frippery.org/files/busybox/busybox64u.exe
--- updater.sh
+++ updater.sh
@@ -5,12 +5,25 @@ set -e
 GITHUB_TAGS_URL="https://github.com/amzxyz/rime_wanxiang/tags"
 GITHUB_RELEASES_URL="https://github.com/amzxyz/rime_wanxiang/releases"
 
+WHITE_LIST_FILES="简纯+.trime.yaml default.yaml squirrel.yaml weasel.yaml"
+
+echo_red() {
+  printf '\033[31m%s\033[0m\n' "$*"
+}
+echo_yellow() {
+  printf '\033[33m%s\033[0m\n' "$*"
+}
+echo_green() {
+  printf '\033[32m%s\033[0m\n' "$*"
+}
+
+
 get_latest_wanxiang_version() {
     latest_version="$(curl -s $GITHUB_TAGS_URL | grep -E "tags/v[0-9]+" | awk -F 'tags/' '{print $2}' | grep '.zip' | awk -F '.zip' '{print $1}' | head -n 1)"
 }
 
 get_local_wanxiang_version() {
-    local_version="$(cat "$TARGET_DIR/wanxiang-version.txt")" || local_version="v0.0.0"
+    [ -f "$TARGET_DIR/wanxiang-version.txt" ] && local_version="$(cat "$TARGET_DIR/wanxiang-version.txt")" || local_version="v0.0.0"
 }
 
 compare_versions() {
@@ -34,8 +47,7 @@ define_download_urls() {
 }
 
 ask_target_directory() {
-    echo "Please choose an input method with rime, so we can download"
-    echo "files to correct directory:"
+    echo_green "Please choose an input method with rime, so we can download files to correct directory:"
     echo "1. ibus-rime"
     echo "2. fcitx5-rime"
     echo "3. Weasel | 小狼毫"
@@ -51,7 +63,7 @@ ask_target_directory() {
     4) TARGET_DIR="$HOME/Library/Rime" ;;
     5) TARGET_DIR="/sdcard/rime" ;;
     6) 
-        echo "Please enter the custom dictionary directory: "
+        echo_green "Please enter the custom dictionary directory: "
         read -r TARGET_DIR
         ;;
     *) TARGET_DIR="error" ;;
@@ -59,28 +71,53 @@ ask_target_directory() {
 }
 
 ask_target_edition() {
-    echo "Please choose an input edition, such as Pinyin(全拼)、Wubi(五笔)、"
-    echo "flypy(小鹤双拼) or others, and whether to "
-    echo "use auxiliary solutions:"
-    echo "1. Stand IME (Support Shuangpin) | 标准版输入方案（支持双拼）"
-    echo "2. ZRM auxiliary Enhanced version | 增强版自然码辅助版本"
-    echo "3. Tiger auxiliary Enhanced version | 增强版虎码首末辅助版本"
-    echo "4. Moqi auxiliary Enhanced version | 增强版墨奇辅助版本"
-    echo "5. Xiaohe(flypy) auxiliary Enhanced version | 增强版小鹤辅助版本"
-    echo "6. Wubi | 五笔"
-    echo "7. Hanxin auxiliary Enhanced version | 增强版汉芯辅助版本"
+    notice_stand="Stand IME (Support Shuangpin) | 标准版输入方案（支持双拼）"
+    notice_zrm="ZRM auxiliary Enhanced version | 增强版自然码辅助版本"
+    notice_tiger="Tiger auxiliary Enhanced version | 增强版虎码首末辅助版本"
+    notice_moqi="Moqi auxiliary Enhanced version | 增强版墨奇辅助版本"
+    notice_xiaohe="Xiaohe(flypy) auxiliary Enhanced version | 增强版小鹤辅助版本"
+    notice_wubi="Wubi | 五笔"
+    notice_hanxin="Hanxin auxiliary Enhanced version | 增强版汉芯辅助版本"
+    echo_green "Please choose an input edition, such as Pinyin(全拼)、Wubi(五笔)、flypy(小鹤双拼) or others, and whether to use auxiliary solutions:"
+    echo "1. $notice_stand"
+    echo "2. $notice_zrm"
+    echo "3. $notice_tiger"
+    echo "4. $notice_moqi"
+    echo "5. $notice_xiaohe"
+    echo "6. $notice_wubi"
+    echo "7. $notice_hanxin"
     echo "Please enter the edition number: "
     read -r EDITION
     case $EDITION in
-    1) SELECTED_EDITION="stand" ;;
-    2) SELECTED_EDITION="zrm" ;;
-    3) SELECTED_EDITION="tiger" ;;
-    4) SELECTED_EDITION="moqi" ;;
-    5) SELECTED_EDITION="xiaohe" ;;
-    6) SELECTED_EDITION="wubi" ;;
-    7) SELECTED_EDITION="hanxin" ;;
+    1) SELECTED_EDITION="stand" && notice=$notice_stand ;;
+    2) SELECTED_EDITION="zrm" && notice=$notice_zrm ;;
+    3) SELECTED_EDITION="tiger" && notice=$notice_tiger ;;
+    4) SELECTED_EDITION="moqi" && notice=$notice_moqi ;;
+    5) SELECTED_EDITION="xiaohe" && notice=$notice_xiaohe ;;
+    6) SELECTED_EDITION="wubi" && notice=$notice_wubi ;;
+    7) SELECTED_EDITION="hanxin" && notice=$notice_hanxin ;;
     *) SELECTED_EDITION="error" ;;
     esac
+    [ -f "$TARGET_DIR/wanxiang-edition.txt" ] && local_edition="$(cat "$TARGET_DIR/wanxiang-edition.txt")" || local_edition="none"
+    if [ "$local_edition" != "none" ]; then
+        case $local_edition in
+            "stand") notice_old=$notice_stand ;;
+            "zrm") notice_old=$notice_zrm ;;
+            "tiger") notice_old=$notice_tiger ;;
+            "moqi") notice_old=$notice_moqi ;;
+            "xiaohe") notice_old=$notice_xiaohe ;;
+            "wubi") notice_old=$notice_wubi ;;
+            "hanxin") notice_old=$notice_hanxin ;;
+            *) notice_old="broken" ;;
+        esac
+    fi
+    if [ "$local_edition" != "none" ] && [ "$SELECTED_EDITION" != "$local_edition" ]; then
+        echo_yellow "You have selected a different edition than the current one ($notice_old), The current edition will be replaced with the new one ($notice). Continue? (y/n)"
+        read -r CONFIRM
+        if [ "$CONFIRM" != "y" ]; then
+            SELECTED_EDITION="error"
+        fi
+    fi
 }
 
 ask_user() {
@@ -116,15 +153,21 @@ define_target_urls() {
 
 install_wanxiang() {
     temp_dir=$(mktemp -d -t "wanxiang-XXXXXXX")
+    echo_green "Downloading $TARGET_URL"
     curl -# -L -o "$temp_dir/wanxiang.zip" "$TARGET_URL"
+    echo_green "Downloading $LANGUAGE_MODULE"
     curl -# -L -o "$temp_dir/wanxiang-lts-zh-hans.gram" "$LANGUAGE_MODULE"
     [ -d "$TARGET_DIR" ] || mkdir -p "$TARGET_DIR"
     [ -f "$TARGET_DIR/wanxiang-version.txt" ] && rm -f "$TARGET_DIR/wanxiang-version.txt"
     [ -f "$TARGET_DIR/wanxiang-lts-zh-hans.gram" ] && rm -f "$TARGET_DIR/wanxiang-lts-zh-hans.gram"
-    unzip -o "$temp_dir/wanxiang.zip" -d "$TARGET_DIR"
+    unzip -o "$temp_dir/wanxiang.zip" -d "$TARGET_DIR" -x $WHITE_LIST_FILES
+    for file in $WHITE_LIST_FILES; do
+        [ ! -f "$TARGET_DIR/$file" ] && unzip "$temp_dir/wanxiang.zip" "$file" -d "$TARGET_DIR/$file"
+    done
     cp "$temp_dir/wanxiang-lts-zh-hans.gram" "$TARGET_DIR"/wanxiang-lts-zh-hans.gram
     rm -rf "$temp_dir"
     echo "$latest_version" > "$TARGET_DIR/wanxiang-version.txt"
+    echo "$SELECTED_EDITION" > "$TARGET_DIR/wanxiang-edition.txt"
 }
 
 main() {    
